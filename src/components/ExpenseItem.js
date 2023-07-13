d84341f59aff1973e492115ea903b25988ba9ac6
abhi620195 <abhi.arya97161>
2023-07-13 22:07:27 +0530

    first commit

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,11 @@
+function ExpenseItem(){
+    return(
+        <div>
+            <div><h2>Food</h2>Rs.10</div>
+            <div><h2>Petrol</h2>Rs.100</div>
+            <div><h2>Movies</h2>Rs.200</div>
+        </div>
+    )
+}
+
+export default ExpenseItem;
