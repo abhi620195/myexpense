87fb8245cfe3a5a5407a7241b96323987e724d44
abhi620195 <abhi.arya97161>
2023-07-14 21:33:19 +0530

    On click amount change to $100

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,18 +1,26 @@
+import React,{ useState } from 'react';
 import './ExpenseItem.css';
 import ExpenseDate from './ExpenseDate';
 import ExpenseDetails from './ExpenseDetails'
 import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
+    const [amount,setTitle] = useState(props.amount);
 
-    const deleteExpense = () =>{
+    const clickHandler = () =>{
+        setTitle("100");
+        // console.log(title);
     }
 
     return(
         <Card className='expense-item'>
             <ExpenseDate date={props.date} />
-            <ExpenseDetails amount={props.amount}  LocationOfExpenditure={props.LocationOfExpenditure}   title={props.title} />
-            <button onClick={deleteExpense}>Delete Expenses</button>
+            <div className='expense-item__description'>
+                <h2>{props.title}</h2>
+                <div className='expense-item__price'>${amount}</div>
+
+            </div>
+            <button onClick = { clickHandler }>Change Amount</button>
         </Card>
     )
 }
