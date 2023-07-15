a3440dd3d73363fa861f80c80d14d8d4538daded
abhi620195 <abhi.arya97161>
2023-07-15 11:25:58 +0530

    Add expense working and visible on screen

--- src/App.js
+++ src/App.js
@@ -1,47 +1,49 @@
 // import ExpenseItem from './components/ExpenseItem';
 import Expenses from './components/Expenses/Expenses';
 import NewExpense from './components/NewExpense/NewExpense';
+import React, {useState} from 'react';
+
+let DUMMY_EXPENSE = [
+  {
+    id: 'e1',
+    title: 'Toilet Paper',
+    amount: 94.12,
+    date: new Date(2020, 7, 14),
+    LocationOfExpenditure : 'Mumbai',
+  },
+
+  {
+    id: 'e2',
+    title: 'New TV',
+    amount: 794.12,
+    date: new Date(2020, 7, 12),
+    LocationOfExpenditure : 'Delhi',
+  },
+
+  {
+    id: 'e3',
+    title: 'Car Insurance',
+    amount: 294.12,
+    date: new Date(2020, 2, 28),
+    LocationOfExpenditure : 'Banglore',
+  },
+
+  {
+    id: 'e4',
+    title: 'Wooden',
+    amount: 450,
+    date: new Date(2021, 5, 12),
+    LocationOfExpenditure : 'Bihar',
+  },
+];
 
 
 const App = () => { 
 
-  const expenses = [
-    {
-      id: 'e1',
-      title: 'Toilet Paper',
-      amount: 94.12,
-      date: new Date(2020, 7, 14),
-      LocationOfExpenditure : 'Mumbai',
-    },
-
-    {
-      id: 'e2',
-      title: 'New TV',
-      amount: 794.12,
-      date: new Date(2020, 7, 12),
-      LocationOfExpenditure : 'Delhi',
-    },
-
-    {
-      id: 'e3',
-      title: 'Car Insurance',
-      amount: 294.12,
-      date: new Date(2020, 2, 28),
-      LocationOfExpenditure : 'Banglore',
-    },
-
-    {
-      id: 'e4',
-      title: 'Wooden',
-      amount: 450,
-      date: new Date(2021, 5, 12),
-      LocationOfExpenditure : 'Bihar',
-    },
-];
-
+  const [expenses, setExpenses] = useState(DUMMY_EXPENSE);
   const addExpenseHandler = expense => {
-    // console.log("IN APP.JS");
-    // console.log(expense);
+    const updatedExpense = [expense,...expenses];
+    setExpenses(updatedExpense);
   }
 
   return (
