94adacb7d3a7b7d7d6a09dccc8555d177720fe58
abhi620195 <abhi.arya97161>
2023-07-15 09:36:39 +0530

    on the submit button create an object

--- src/App.js
+++ src/App.js
@@ -39,10 +39,14 @@ const App = () => {
     },
 ];
 
+  const addExpenseHandler = expense => {
+    // console.log("IN APP.JS");
+    // console.log(expense);
+  }
 
   return (
     <div>
-      <NewExpense />
+      <NewExpense onAddExpense = {addExpenseHandler}/>
       <Expenses items={expenses} />
     </div>
   )
