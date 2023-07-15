80a3a45b18d49468d14e3870afac2a134dd085ba
abhi620195 <abhi.arya97161>
2023-07-15 16:20:49 +0530

    Removing Default values

--- src/App.js
+++ src/App.js
@@ -4,37 +4,37 @@ import NewExpense from './components/NewExpense/NewExpense';
 import React, {useState} from 'react';
 
 let DUMMY_EXPENSE = [
-  {
-    id: 'e1',
-    title: 'Toilet Paper',
-    amount: 94.12,
-    date: new Date(2020, 7, 14),
-    LocationOfExpenditure : 'Mumbai',
-  },
+//   {
+//     id: 'e1',
+//     title: 'Toilet Paper',
+//     amount: 94.12,
+//     date: new Date(2020, 7, 14),
+//     LocationOfExpenditure : 'Mumbai',
+//   },
 
-  {
-    id: 'e2',
-    title: 'New TV',
-    amount: 794.12,
-    date: new Date(2020, 7, 12),
-    LocationOfExpenditure : 'Delhi',
-  },
+//   {
+//     id: 'e2',
+//     title: 'New TV',
+//     amount: 794.12,
+//     date: new Date(2020, 7, 12),
+//     LocationOfExpenditure : 'Delhi',
+//   },
 
-  {
-    id: 'e3',
-    title: 'Car Insurance',
-    amount: 294.12,
-    date: new Date(2020, 2, 28),
-    LocationOfExpenditure : 'Banglore',
-  },
+//   {
+//     id: 'e3',
+//     title: 'Car Insurance',
+//     amount: 294.12,
+//     date: new Date(2020, 2, 28),
+//     LocationOfExpenditure : 'Banglore',
+//   },
 
-  {
-    id: 'e4',
-    title: 'Wooden',
-    amount: 450,
-    date: new Date(2021, 5, 12),
-    LocationOfExpenditure : 'Bihar',
-  },
+//   {
+//     id: 'e4',
+//     title: 'Wooden',
+//     amount: 450,
+//     date: new Date(2021, 5, 12),
+//     LocationOfExpenditure : 'Bihar',
+//   },
 ];
 
 
